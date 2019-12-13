40f0944fd81a7aa723fac617e83128d13f151b1e
Joaomon7eiro <joaoagrom@gmail.com>
2019-12-13 16:11:15 -0300

    dist removed

--- dist/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});var _express = require('express');
-
-const routes = _express.Router.call(void 0, );
-
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
-});
-
-exports. default = routes;
